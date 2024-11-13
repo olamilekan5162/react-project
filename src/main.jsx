38998b975f145909001ace01607e67875955019d
opeyemi olalekan <opeyemiolalekan481@gmail.com>
2024-11-13 18:15:34 +0100

    fix the follow come css issue

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
 import Shopping from './Shopping.jsx'
-import Counter from './Counter.jsx'
+
 
 
 createRoot(document.getElementById('root')).render(
